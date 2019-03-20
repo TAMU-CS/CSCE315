78c67ac8633fbb4b0984334877c7bcb8be56055a
gilliangacusan <gvgacusan310@gmail.com>
2019-03-19 20:01:10 -0500

    added printBoard and getting player scores

--- src/Board.java
+++ src/Board.java
@@ -1,15 +1,11 @@
 
 public class Board {
-	private int[][] board = new int[2][8]; // 2 rows, 6 houses + 2 mancala house per player
+	private int[][] board = new int[2][8]; // 2 rows, 6 houses + 2 mancala houses
 	
 	private Player[] players = new Player[2]; 
 	
 	public Board() {
-		for(int i = 0; i < 2; i++) {
-			for(int j = 1; j < 7; j++) {
-				board[i][j] = 4; // each house starts with 4 seeds
-			}
-		}
+		// init board
 		
 		players[0] = new Player();
 		players[1] = new Player();
