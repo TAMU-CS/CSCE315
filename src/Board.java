6c5886be92828d39c4e0bf8190bfef8d355d374a
Kevin Tang <kevin26428@gmail.com>
2019-03-20 09:34:21 -0500

    Setting up board implementation

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,19 @@
 
 public class Board {
-	private int[][] board = new int[2][8]; // 2 rows, 6 houses + 2 mancala houses
 	
-	private Player[] players = new Player[2]; 
+	//board data structure, 2x6 board to represent houses for seeds
+	private int[][] board = new int[2][6];
 	
+	//score datastructure for keep track of scores
+	private int[] score = new int[2];
+	private int playerturn;
+	
+	//player objects, correspond with their score index
+	private Player[] players = new Player[2];
+	
+	/* 
+	 * Default constructor, initiates an empty kalah board with players
+	 */
 	public Board() {
 		// init board
 		
@@ -11,6 +21,9 @@ public class Board {
 		players[1] = new Player();
 	}
 	
+	/*
+	 * Print board, prints out the current kalah board state
+	 */
 	public void printBoard() {
 		for(int i = 0; i < 2; i++) {
 			for(int j = 0; j < 8; j++) {
@@ -24,4 +37,14 @@ public class Board {
 		System.out.print("User: " + players[0].getScore() + "\n");
 		System.out.print("AI: " + players[1].getScore());
 	}
+	
+	/*
+	 * AddScore takes player index, and increments their score by 1
+	 */
+	public void AddScore(int index) {
+		
+	}
+	
+	
+	
 }
