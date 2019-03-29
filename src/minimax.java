cfece6eaabcdbc1a7ff78ea399608542c1805792
Gillian G <gvgacusan310@gmail.com>
2019-03-29 16:58:28 -0500

    added minimax functionality

--- src/minimax.java
+++ src/minimax.java
@@ -1,12 +1,39 @@
+import java.util.*;
 
-int MAX = 1000;
-int MIN = -1000;
+public int miniMax(int depth, boolean isMaximizingPlayer, int playerturn, int[][] boardState) {
+  // Get possible next moves
+  int[][] nextMoves = GetMoves(playerturn);
 
-public int minimax(int depth, int nodeIndex, boolean maxPlayer,
-                    int board[], int alpha, int beta, int playerTurn) {
-  // Reached leaf
-  if(depth == 3) {
-    return board
+  int bestScore = (isMaximizingPlayer) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+  int currentScore;
+  int bestRow = -1;
+  int bestCol = -1;
+
+  if(nextMoves.isEmpty() || depth == 0) {
+    System.out.println("Game over");
+  } else {
+    for(int[] move: nextMoves) {
+
+      // figure out best move for this player
+      boardState[[move[0]][move[1]]] = playerturn;
+
+      if(isMaximizingPlayer) { // AI is the maximizing player
+        currentScore = miniMax(depth - 1, true, 1);
+        if(currentScore > bestScore) {
+          bestScore = currentScore;
+          bestRow = move[0];
+          bestCol = move[1];
+        } else { // Human player
+          currentScore = miniMax(depth - 1, false, 0);
+          if(currentScore < bestScore) {
+            bestRow = move[0];
+            bestCol = move[1];
+          }
+        }
+        // Undo move
+        boardState[[move[0]][move[1]]] = -1;
+      }
+    }
   }
-
+  return new int[] {bestScore, bestRow, bestCol};
 }
